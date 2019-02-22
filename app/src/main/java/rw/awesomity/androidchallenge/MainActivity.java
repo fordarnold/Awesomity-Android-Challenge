3c2f7c618da87b7b6e8b0afc6443a89b64bf37c9
Robin Hood <fordarnold@gmail.com>
2019-02-22 15:26:25 +0200

    Completed tutorial ~ http://www.sanktips.com/2017/12/10/how-to-fetch-json-data-using-volley-and-display-it-to-recyclerview-in-android-studio/
    Signed-off-by: Robin Hood <fordarnold@gmail.com>

--- app/src/main/java/rw/awesomity/androidchallenge/MainActivity.java
+++ app/src/main/java/rw/awesomity/androidchallenge/MainActivity.java
@@ -4,6 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -14,6 +29,14 @@ public class MainActivity extends Activity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter recyclerViewAdapter;
     private RecyclerView.LayoutManager layoutManager;
+
+    List<Character> characterList;
+
+    // Setup a Request Queue
+    RequestQueue requestQueue;
+
+    // Set the URL string where data is retrieved from
+    String url = "https://awesomity.rw/api/sample/challenge/data/json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +58,58 @@ public class MainActivity extends Activity {
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
+        /**
+         * Send http request using Volley
+         */
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        characterList = new ArrayList<>();
+
+        sendRequest();
+
         // Set an adapter to pull data for the RecyclerView
 //        recyclerViewAdapter = new MarvelAdapter(dataset);
 //        recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    public void sendRequest(){
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                for(int i = 0; i < response.length(); i++){
+
+                    Character ch = new Character();
+
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+
+                        ch.setName(jsonObject.getString("name"));
+                        ch.setLocation(jsonObject.getString("location"));
+                        ch.setPowers(jsonObject.getString("powers"));
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    characterList.add(ch);
+
+                }
+
+                recyclerViewAdapter = new MarvelAdapter(MainActivity.this, characterList);
+
+                recyclerView.setAdapter(recyclerViewAdapter);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("Volley Error: ", error.toString());
+            }
+        });
+
+        requestQueue.add(jsonArrayRequest);
     }
 }
--- app/src/main/java/rw/awesomity/androidchallenge/MarvelAdapter.java
+++ app/src/main/java/rw/awesomity/androidchallenge/MarvelAdapter.java
@@ -1,14 +1,19 @@
 package rw.awesomity.androidchallenge;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MarvelAdapter extends RecyclerView.Adapter<MarvelAdapter.MyViewHolder> {
 
-    private String[] myDataset;
+    private Context context;
+    private List<Character> characterList;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
@@ -16,20 +21,34 @@ public class MarvelAdapter extends RecyclerView.Adapter<MarvelAdapter.MyViewHold
     public static class MyViewHolder extends RecyclerView.ViewHolder {
 
         // each data item is just a string in this case
-        public TextView name, location, powers;
+        public TextView name;
+        public TextView location;
+        public TextView powers;
 
-        public MyViewHolder(View v) {
-            super(v);
+        public MyViewHolder(View itemView) {
+            super(itemView);
 
-            name = (TextView) v.findViewById(R.id.txt_name);
-            location = (TextView) v.findViewById(R.id.txt_location);
-            powers = (TextView) v.findViewById(R.id.txt_powers);
+            name = (TextView) itemView.findViewById(R.id.txt_name);
+            location = (TextView) itemView.findViewById(R.id.txt_location);
+            powers = (TextView) itemView.findViewById(R.id.txt_powers);
+
+            // Define what happens when you click on a single list item
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Character character = (Character) view.getTag();
+
+                    Toast.makeText(view.getContext(), character.getName()+" "+character.getLocation()+" is "+ character.getPowers(), Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public MarvelAdapter(String[] dataset) {
-        myDataset = dataset;
+    public MarvelAdapter(Context context, List characterList) {
+        this.context = context;
+        this.characterList = characterList;
     }
 
     // Create new views (invoked by the layout manager)
@@ -37,10 +56,10 @@ public class MarvelAdapter extends RecyclerView.Adapter<MarvelAdapter.MyViewHold
     public MarvelAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
         // create a new view
-        View itemView = LayoutInflater.from(parent.getContext())
+        View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.list_row_characters, parent, false);
 
-        return new MyViewHolder(itemView);
+        return new MyViewHolder(v);
     }
 
     // Bind the view holder to its data (invoked by the layout manager)
@@ -48,13 +67,19 @@ public class MarvelAdapter extends RecyclerView.Adapter<MarvelAdapter.MyViewHold
     public void onBindViewHolder(MyViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.name.setText(myDataset[position]);
+        holder.itemView.setTag(characterList.get(position));
+
+        Character ch = characterList.get(position);
+
+        holder.name.setText(ch.getName());
+        holder.location.setText(ch.getLocation());
+        holder.powers.setText(ch.getPowers());
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return myDataset.length;
+        return characterList.size();
     }
 
 }
